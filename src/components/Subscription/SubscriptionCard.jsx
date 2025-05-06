161af656b4aa79c403fd779c894e79a174ffa56f
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-06 18:22:00 +0600

    subscription part done

---
+++ src/components/Subscription/SubscriptionCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const SubscriptionCard = ({ service }) => {
+  return (
+    <div className="card lg:max-w-lg bg-base-100 shadow-sm">
+      <figure>
+        <img
+          src={service.thumbnail}
+          alt={service.name}
+          className="w-full h-48 object-cover"
+        />
+      </figure>
+      <div className="card-body">
+        <span className="badge badge-md badge-warning">{service.category}</span>
+        <div className="flex justify-between items-center">
+          <h2 className="text-xl font-bold">{service.name}</h2>
+          <span className="text-base font-semibold">
+            {service.price}/{service.frequency}
+          </span>
+        </div>
+
+        <div className="mt-4">
+          <button className="btn btn-primary btn-block">View More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Check = ({ disabled = false }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className={`size-4 me-2 inline-block ${
+      disabled ? "text-base-content/50" : "text-success"
+    }`}
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M5 13l4 4L19 7"
+    />
+  </svg>
+);
+
+export default SubscriptionCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import Subscription from "../components/Subscription/Subscription";
 
 const Home = () => {
   return (
     <div>
       <Hero></Hero>
+      <Subscription></Subscription>
     </div>
   );
 };
