80bd3c1c23f6b111a5f1c60981de1fa44e3b35e3
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-07 07:59:14 +0600

    routes code organized

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 // src/App.jsx
 import React, { useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
 
-import Navbar from "../src/components/Shared/Navbar";
-import Home from "./pages/Home";
-import Products from "./components/Products";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import Navbar from "./components/Shared/Navbar";
 import Footer from "./components/Shared/Footer";
-import SubscriptionDetail from "./components/Subscription/SubscriptionDetails";
-import Blogs from "./components/Blogs/Blogs";
-import NotFound from "./components/NotFound/NotFound";
-import Profile from "./components/Profile/Profile";
-import ForgotPassword from "./components/ForgetPassword";
-import Contact from "./components/Contact/Contact";
-import SuccessStories from "./components/Success-Stories/Success";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -32,55 +20,8 @@ function App() {
   return (
     <>
       <Navbar user={user} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/blogs"
-          element={
-            <ProtectedRoute user={user}>
-              <Blogs></Blogs>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute user={user}>
-              <Profile></Profile>
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/success" element={<SuccessStories />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/products"
-          element={
-            <ProtectedRoute user={user}>
-              <Products />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/subscriptions/:id"
-          element={
-            <ProtectedRoute user={user}>
-              <SubscriptionDetail />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={!user ? <Login /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/register"
-          element={!user ? <Register /> : <Navigate to="/" />}
-        />
-        <Route path="*" element={<NotFound></NotFound>} />
-      </Routes>
-
-      <Footer></Footer>
+      <AppRoutes user={user} />
+      <Footer />
     </>
   );
 }
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,64 @@
+// src/routes/AppRoutes.jsx
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import Products from "../components/Products";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import Blogs from "../components/Blogs/Blogs";
+import NotFound from "../components/NotFound/NotFound";
+import Profile from "../components/Profile/Profile";
+import ForgotPassword from "../components/ForgetPassword";
+import Contact from "../components/Contact/Contact";
+import SuccessStories from "../components/Success-Stories/Success";
+import SubscriptionDetail from "../components/Subscription/SubscriptionDetails";
+import ProtectedRoute from "../pages/ProtectedRoute";
+
+export default function AppRoutes({ user }) {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/blogs"
+        element={
+          <ProtectedRoute user={user}>
+            <Blogs />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute user={user}>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/success" element={<SuccessStories />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route
+        path="/products"
+        element={
+          <ProtectedRoute user={user}>
+            <Products />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/subscriptions/:id"
+        element={
+          <ProtectedRoute user={user}>
+            <SubscriptionDetail />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+      <Route
+        path="/register"
+        element={!user ? <Register /> : <Navigate to="/" />}
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
