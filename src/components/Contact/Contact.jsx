54b7694dc401834ac384efb91df8358cffe02b76
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-13 23:41:52 +0600

    phone number updated and email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -34,10 +34,10 @@ const Contact = () => {
                     />
                   </svg>
                   <p class="mt-6 text-lg font-medium text-gray-900">
-                    +8801757532012
+                    +8801540026119
                   </p>
                   <p class="mt-1 text-lg font-medium text-gray-900">
-                    +88010002634543
+                    +8801869492183
                   </p>
                 </div>
               </div>
@@ -59,7 +59,7 @@ const Contact = () => {
                     />
                   </svg>
                   <p class="mt-6 text-lg font-medium text-gray-900">
-                    contact@gmail.com
+                    anik.haque.cse1@gmail.com
                   </p>
                   <p class="mt-1 text-lg font-medium text-gray-900">
                     test@gmail.com
