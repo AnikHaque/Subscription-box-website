adf3dbf95b5a5481c4002becfa028f2506da8eec
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-15 15:47:53 +0600

    all

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -163,7 +163,7 @@ const Blogs = () => {
   return (
     <div className="px-4 py-10 max-w-7xl mx-auto">
       <h2 className="text-3xl lg:text-5xl font-bold text-center mb-20">
-        Our All Blogs
+        Our All Blogss
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
